b29975eb884c9d48416fc8fff5d058768e90305a
Vika <kaledaviktoriya@gmail.com>
2018-09-28 14:08:30 +0300

    Add SignalR to project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlogHosting.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
